656e22bd0a691373046d1454ff50d53a469abd21
J1uShan9 <jiushang1124@gmail.com>
2024-06-29 11:19:11 +0800

    WaitAvailableAction log

--- util/Task/__init__.py
+++ util/Task/__init__.py
@@ -192,9 +192,27 @@ class Task:
         等待开票
         """
         interval = abs(self.api.GetSaleStartTime() - int(time())) - 30
-        logger.info(f"【等待开票】需要等待 {interval/60:.1f} 分钟")
-        sleep(interval)
-        logger.info("【等待开票】等待结束! 开始抢票")
+        countdown = interval
+
+        if countdown >= 3600:
+            for _ in range(countdown//10):
+                logger.info(f"【等待开票】需要等待 {countdown/60:.1f} 分钟")
+                sleep(600)
+                countdown -= 600
+        if 3600 > countdown >= 600: 
+            for _ in range(10):
+                logger.info(f"【等待开票】即将开票! 需要等待 {countdown/60:.1f} 分钟")
+                sleep(60)
+                countdown -=60
+        if 600 > countdown >= 60: 
+            for _ in range(12):
+                logger.info(f"【等待开票】准备开票! 需要等待 {countdown/60:.1f} 分钟")
+                sleep(5)
+                countdown -=5
+        if countdown == 0:
+            logger.info("【等待开票】等待结束! 开始抢票")
+            
+            
 
     @logger.catch
     def RiskProcessAction(self) -> None:
