5fa53b2b700ab28afc11dfe1e383fbea29acdf5e
J1uShan9 <jiushang1124@gmail.com>
2024-06-30 19:22:54 +0800

    fix

--- interface/CLI/product.py
+++ interface/CLI/product.py
@@ -69,7 +69,7 @@ class ProductCli:
             """
             活动
             """
-            print(f"{self.BLUE}[{self.YELLOW}!{self.BLUE}]{self.RESET} BW2024链接: show.bilibili.com/platform/detail.html?id=85939"
+            print(f"{self.BLUE}[{self.YELLOW}!{self.BLUE}]{self.RESET} BW2024链接: show.bilibili.com/platform/detail.html?id=85939")
             print(f"{self.BLUE}[{self.YELLOW}!{self.BLUE}]{self.RESET} BML2024链接: show.bilibili.com/platform/detail.html?id=85938")
             url = self.data.Inquire(
                 type="Text",
