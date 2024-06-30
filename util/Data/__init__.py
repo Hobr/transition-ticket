bbb57a02b5e99d71dc746205521050650969385c
Hobr <mail@hobr.site>
2024-06-30 22:33:07 +0800

    format

--- util/Data/__init__.py
+++ util/Data/__init__.py
@@ -21,18 +21,18 @@ class CustomThemes(GreenPassion):
     def __init__(self):
         super().__init__()
 
-        self.List.selection_cursor = "->"  # 选择光标
-        self.Checkbox.selection_icon = "->"  # 选择光标
+        self.List.selection_cursor = "->"  # 选择光标 # type: ignore
+        self.Checkbox.selection_icon = "->"  # 选择光标 # type: ignore
 
-        self.Checkbox.selected_icon = "✔"  # Checkbox选项 的启用图标
-        self.Checkbox.unselected_icon = "✘"  # Checkbox选项 的未启用图标
-        self.Checkbox.selection_color = f"\033[1;35;106m"  # Checkbox选项 的选中颜色(紫，蓝)
-        self.Checkbox.selected_color = f"\033[93m"  # Checkbox选项 的启用颜色(黄)
+        self.Checkbox.selected_icon = "✔"  # Checkbox选项 的启用图标 # type: ignore
+        self.Checkbox.unselected_icon = "✘"  # Checkbox选项 的未启用图标 # type: ignore
+        self.Checkbox.selection_color = "\033[1;35;106m"  # Checkbox选项 的选中颜色(紫，蓝) # type: ignore
+        self.Checkbox.selected_color = "\033[93m"  # Checkbox选项 的启用颜色(黄) # type: ignore
 
-        self.List.selection_color = f"\033[1;35;106m"  # List选项 的选中颜色(紫，蓝)
+        self.List.selection_color = "\033[1;35;106m"  # List选项 的选中颜色(紫，蓝) # type: ignore
 
-        self.Question.mark_color = f"\033[93m"  # [?] 中 ? 的颜色(黄)
-        self.Question.brackets_color = f"\033[96m"  # [?] 中 [] 的颜色(蓝)
+        self.Question.mark_color = "\033[93m"  # [?] 中 ? 的颜色(黄) # type: ignore
+        self.Question.brackets_color = "\033[96m"  # [?] 中 [] 的颜色(蓝) # type: ignore
 
 
 class Data:
