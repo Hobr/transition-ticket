7a739dd1a0ab7f283b03b9130cdcf9002afbbeb0
J1uShan9 <jiushang1124@gmail.com>
2024-07-01 15:43:33 +0800

    cli whileTrue 成功抢票后返回

--- cli.py
+++ cli.py
@@ -18,89 +18,92 @@ def cleanup_meipass() -> None:
         except Exception as e:
             print(f"清理失败 {meipass_path}: {e}")
 
-
 atexit.register(cleanup_meipass)
 
 if __name__ == "__main__":
-    # 丢锅
-    print(
-        """
-|=====================================================================
-|
-|  欢迎使用 https://github.com/biliticket/transition-ticket
-|  本程序仅供学习交流, 不得用于商业用途
-|  使用本程序进行违法操作产生的法律责任由操作者自行承担
-|  对本程序进行二次开发/分发时请注意遵守GPL-3.0开源协议
-|  本脚本仅适用于蹲回流票, 我们反对将其用于抢票
-|  黄牛４０００＋
-|
-|=====================================================================
-|
-|  交互: 上下 键盘↑↓键, 多选 空格, 确认 回车
-|
-|=====================================================================
-"""
-    )
+    while True:
 
-    # 日志
-    logger.add(
-        "log/{time}.log",
-        colorize=False,
-        enqueue=True,
-        encoding="utf-8",
-        # 调试
-        backtrace=True,
-        diagnose=True,
-    )
+        # 丢锅
+        print(
+            """
+    |=====================================================================
+    |
+    |  欢迎使用 https://github.com/biliticket/transition-ticket
+    |  本程序仅供学习交流, 不得用于商业用途
+    |  使用本程序进行违法操作产生的法律责任由操作者自行承担
+    |  对本程序进行二次开发/分发时请注意遵守GPL-3.0开源协议
+    |  本脚本仅适用于蹲回流票, 我们反对将其用于抢票
+    |  黄牛４０００＋
+    |
+    |=====================================================================
+    |
+    |  交互: 上下 键盘↑↓键, 多选 空格, 确认 回车
+    |
+    |=====================================================================
+    """
+        )
 
-    # 初始化
-    # 用户数据文件
-    userData = Config(dir="user")
-    productData = Config(dir="product")
-    settingData = Config(dir="setting")
-    # 验证
-    cap = Captcha()
+        # 日志
+        logger.add(
+            "log/{time}.log",
+            colorize=False,
+            enqueue=True,
+            encoding="utf-8",
+            # 调试
+            backtrace=True,
+            diagnose=True,
+        )
 
-    # 检测配置文件情况
-    userList = userData.List()
-    productList = productData.List()
-    settingList = settingData.List()
+        # 初始化
+        # 用户数据文件
+        userData = Config(dir="user")
+        productData = Config(dir="product")
+        settingData = Config(dir="setting")
+        # 验证
+        cap = Captcha()
 
-    # 读取配置
-    userConfig = UserCli(conf=userData).Select(selects=userList) if userList != [] else UserCli(conf=userData).Generate()
-    productConfig = ProductCli(conf=productData).Select(selects=productList) if productList != [] else ProductCli(conf=productData).Generate()
-    settingConfig = SettingCli(conf=settingData).Select(selects=settingList) if settingList != [] else SettingCli(conf=settingData).Generate()
+        # 检测配置文件情况
+        userList = userData.List()
+        productList = productData.List()
+        settingList = settingData.List()
 
-    net = Request(
-        cookie=userConfig["cookie"],
-        header=userConfig["header"],
-        timeout=settingConfig["request"]["timeout"],
-        proxy=settingConfig["request"]["proxy"],
-        isDebug=settingConfig["dev"]["debug"],
-    )
+        # 读取配置
+        userConfig = UserCli(conf=userData).Select(selects=userList) if userList != [] else UserCli(conf=userData).Generate()
+        productConfig = ProductCli(conf=productData).Select(selects=productList) if productList != [] else ProductCli(conf=productData).Generate()
+        settingConfig = SettingCli(conf=settingData).Select(selects=settingList) if settingList != [] else SettingCli(conf=settingData).Generate()
 
-    api = Bilibili(
-        net=net,
-        projectId=productConfig["projectId"],
-        screenId=productConfig["screenId"],
-        skuId=productConfig["skuId"],
-        buyer=userConfig["buyer"],
-        count=len(userConfig["buyer"]),
-        goldTime=settingConfig["request"]["gold"],
-        phone=userConfig["phone"],
-    )
+        net = Request(
+            cookie=userConfig["cookie"],
+            header=userConfig["header"],
+            timeout=settingConfig["request"]["timeout"],
+            proxy=settingConfig["request"]["proxy"],
+            isDebug=settingConfig["dev"]["debug"],
+        )
 
-    job = Task(
-        net=net,
-        cap=cap,
-        api=api,
-        sleep=settingConfig["request"]["sleep"],
-    )
+        api = Bilibili(
+            net=net,
+            projectId=productConfig["projectId"],
+            screenId=productConfig["screenId"],
+            skuId=productConfig["skuId"],
+            buyer=userConfig["buyer"],
+            count=len(userConfig["buyer"]),
+            goldTime=settingConfig["request"]["gold"],
+            phone=userConfig["phone"],
+        )
 
-    # job.DrawFSM()
+        job = Task(
+            net=net,
+            cap=cap,
+            api=api,
+            sleep=settingConfig["request"]["sleep"],
+        )
 
-    # 任务流
-    if job.Run():
+        # job.DrawFSM()
+
+        # 任务流
+        if not job.Run():
+            break
+
         notice = Notice(title="抢票", message="下单成功! 请在十分钟内支付")
         mode = settingConfig["notice"]
         logger.success("【抢票】下单成功! 请在十分钟内支付")
@@ -120,3 +123,5 @@ if __name__ == "__main__":
 
         for t in noticeThread:
             t.start()
+
+        logger.info("【通知】正在重新启动程序...")
